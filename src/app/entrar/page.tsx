fed07112a329d9b6d8b875138120fe4209349257
LuizGustavoNascimeto <luizgnf1@gmail.com>
2025-01-23 10:53:04 -0300

    fixes: /entrar

--- src/app/entrar/page.tsx
+++ src/app/entrar/page.tsx
@@ -1,7 +1,15 @@
-'use client'
+"use client";
 
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { userSchema } from "@/lib/schemas/userSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -9,52 +17,69 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 export default function Entrar() {
-
   const form = useForm<Zod.infer<typeof userSchema>>({
     resolver: zodResolver(userSchema),
     defaultValues: {
       username: "",
     },
-  })
+  });
+
+  const onSubmit = (values: Zod.infer<typeof userSchema>) => {
+    console.log(values);
+  };
 
   return (
     <div className="flex justify-center items-center h-screen bg-white">
       <div className="w-3/5 h-3/4 bg-background rounded-2xl flex justify-between overflow-hidden">
-        <div className="w-2/5 h-full bg-primary flex flex-col justify-center items-center">
-          <h1 className="font-bold text-4xl text-white">Ferramenta</h1>
-          <h1 className="font-bold text-4xl text-white">PEWS</h1>
+        <div className="w-2/5 h-full bg-primary flex flex-col justify-between items-center py-40">
+          <div className="">
+            <h1 className="font-bold text-4xl text-white px-20 text-center">
+              Ferramenta PEWS
+            </h1>
+          </div>
+          <div className="flex  flex-col text-white gap-1">
+            <div className="">Não possui conta?</div>
+            <Button variant={"outline"} className="border-white">Criar conta</Button>
+          </div>
         </div>
 
         <div className="flex flex-col justify-center items-start p-10 w-3/5 gap-10">
-          <h1 className="font-semibold text-2xl">Entrar</h1>
+          <h1 className="font-semibold text-4xl text-primary w-full text-center">Entrar</h1>
           <div className="w-full">
             <Form {...form}>
-              <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel> Nome </FormLabel>
-                  <FormControl>
-                    <Input placeholder="Jordana da silva" {...field} />
-                  </FormControl>
-                  </FormItem>
-                )}
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-4"
+              >
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel> Nome </FormLabel>
+                      <FormControl>
+                        <Input placeholder="Jordana da silva" {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )}
                 />
                 <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel> Senha </FormLabel>
-                  <FormControl>
-                    <Input placeholder="********" {...field} />
-                  </FormControl>
-                  <FormDescription>Esqueceu a senha?</FormDescription>
-                  </FormItem>
-                )}
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel> Senha </FormLabel>
+                      <FormControl>
+                        <Input placeholder="********" {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )}
                 />
-                <Button type="submit" > Entrar</Button>
+                <div className="flex flex-row justify-between items-center">
+                  <span className="opacity-70">Esqueceu sua senha</span>
+                  <Button type="submit"> Entrar</Button>
+                </div>
+              </form>
             </Form>
           </div>
         </div>
@@ -62,4 +87,3 @@ export default function Entrar() {
     </div>
   );
 }
- 
