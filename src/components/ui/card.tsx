a7477fffc6566222d6d1406dda87709b1aa9223a
LuizGustavoNascimeto <luizgnf1@gmail.com>
2025-02-20 10:11:28 -0300

    smallfixes :)

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -4,24 +4,30 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 interface CardProps {
-  name: string,
-  dateAvaluation?: Date,
-  pewsPontuation?: number,
-  patientId?: string,
-
+  name: string;
+  dateAvaluation?: Date;
+  pewsPontuation?: number;
+  patientId?: string;
 }
 
-export default function Card({name, dateAvaluation, pewsPontuation, patientId} : CardProps) {
+export default function Card({
+  name,
+  dateAvaluation,
+  pewsPontuation,
+  patientId,
+}: CardProps) {
   return (
-    <div className="grid grid-cols-4 place-items-center rounded-md bg-card py-3 px-5">
-      <h1 className="justify-self-start">{name}</h1>
-      <h2>{dateAvaluation?.toDateString()}</h2>
-      <h2>{pewsPontuation}</h2>
-      <Link href={`/Pacientes/${patientId}`} className="justify-self-end">
-        <Button className="w-fit">
-          <Settings></Settings>
-        </Button>
-      </Link>
-    </div>
-  )
-};
+    <Link href={`/Pacientes/${patientId}`} className="justify-self-end">
+      <div className="grid grid-cols-4 place-items-center rounded-md bg-card py-3 px-5">
+        <h1 className="justify-self-start">{name}</h1>
+        <h2>{dateAvaluation?.toDateString()}</h2>
+        <h2>{pewsPontuation}</h2>
+        <div className="justify-self-end">
+          <Button className="w-fit">
+            <Settings />
+          </Button>
+        </div>
+      </div>
+    </Link>
+  );
+}
